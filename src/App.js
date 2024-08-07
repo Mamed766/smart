aaaef38e60016d2fb9d7df7b7f2d4cac08cd0187
Mamed766 <mehemmeddemirli57@gmail.com>
2024-08-07 13:27:56 +0400

    feat: Header created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from "react";
+import Header from "./featured/Header/Header";
 
 const App = () => {
-  return <div>Appaaaaa</div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
