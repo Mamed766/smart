b77b15fb07c02893e6206b1bfcbe91677ebe5cc6
Mamed766 <mehemmeddemirli57@gmail.com>
2024-08-08 13:42:07 +0400

    style: cards fixed

--- src/molekuls/CustomMiniCard.jsx
+++ src/molekuls/CustomMiniCard.jsx
@@ -16,13 +16,15 @@ const CustomMiniCard = ({
 }) => {
   return (
     <div
-      className={`h-[${cardHeight}rem] w-[${cardWidth}rem] overflow-hidden mt-5`}
+      className="overflow-hidden mt-5"
+      style={{ height: `${cardHeight}rem`, width: `${cardWidth}rem` }}
     >
       <div className="relative">
         <img
           src={image}
           alt=""
-          className={`object-cover h-[${imageHeight}rem] w-[${imageWidth}rem] rounded-lg cursor-pointer hover:opacity-85 duration-300`}
+          className="object-cover rounded-lg cursor-pointer hover:opacity-85 duration-300"
+          style={{ height: `${imageHeight}rem`, width: `${imageWidth}rem` }}
         />
         {rating && (
           <div className="absolute flex justify-center items-center top-0 right-0 p-2 m-2 border-[2px] h-[40px] w-[40px] border-blue-700 text-white rounded-full">
@@ -30,7 +32,7 @@ const CustomMiniCard = ({
           </div>
         )}
         <button
-          className={`absolute bottom-0 w-[130px] rounded text-[13px] text-white ml-5 flex items-center justify-center text-center  ${getTypeBgColor(
+          className={`absolute bottom-0 w-[130px] rounded text-[13px] text-white ml-5 flex items-center justify-center text-center ${getTypeBgColor(
             type
           )}`}
         >
@@ -38,8 +40,8 @@ const CustomMiniCard = ({
         </button>
       </div>
 
-      <div className="flex flex-col border shadow-lg rounded  p-3">
-        <h2 className="font-bold ">{title}</h2>
+      <div className="flex flex-col border shadow-lg rounded p-3">
+        <h2 className="font-bold">{title}</h2>
 
         <p className="flex items-center gap-1 mt-2 text-[10px]">
           <span>{author}</span>
