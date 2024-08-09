f419dab4f6e5774d3052d1ae69077a96b7fed7ca
Mamed766 <mehemmeddemirli57@gmail.com>
2024-08-09 18:05:29 +0400

    style: responsibility added to footer top

--- src/featured/Footer/Footer.jsx
+++ src/featured/Footer/Footer.jsx
@@ -8,9 +8,9 @@ import {
 const Footer = () => {
   return (
     <footer className=" mt-10">
-      <div className="bg-[#0F0F11]">
+      <div className="bg-[#0F0F11] pt-5">
         <div className="container__default">
-          <div className="flex gap-5 ">
+          <div className="flex items-center  footer__top justify-between w-full  flex-wrap ">
             <div>
               <h2 className="text-white pb-5">Most Popular</h2>
               <div className="flex  flex-col gap-3">
@@ -98,6 +98,36 @@ const Footer = () => {
                     );
                   })}
               </div>
+            </div>
+            <div className="">
+              <form
+                className="flex justify-center mt-4 items-center h-[20rem] border-2 shadow-lg border-gray-900 w-[25rem] bg-black flex-col gap-2"
+                action=""
+              >
+                <h2 className="text-white text-[22px] font-bold">
+                  Subscribe to Updates
+                </h2>
+                <p className="text-gray-400 text-center text-[12px] w-[300px]">
+                  Get the latest creative news from FooBar about art, design and
+                  business.
+                </p>
+                <input
+                  className="bg-[#0F0F11] p-2 w-[300px]"
+                  placeholder="Your email address"
+                  type="text"
+                />
+
+                <div className="flex relative justify-center  items-center  gap-2">
+                  <input type="checkbox" className="mb-5 ml-5 w-[10px]" />
+                  <label className=" w-[300px] text-gray-400  text-[12px] text-center">
+                    By signing up, you agree to the our terms and our Privacy
+                    Policy agreement.
+                  </label>
+                </div>
+                <button className="bg-blue-600 p-3 w-[300px] text-[12px] text-white">
+                  SUBSCRIBE
+                </button>
+              </form>
             </div>
           </div>
         </div>
